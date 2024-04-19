cf752a2ddd7aba4af288f8144010dcd07f1c379f
mellsvs <meltemsavas192@gmail.com>
2024-04-19 15:50:43 +0300

    .

---
+++ src/main/java/net/javaguides/ems/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package net.javaguides.ems.service;
+
+import net.javaguides.ems.dto.EmployeeDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
