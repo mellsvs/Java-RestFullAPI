06dd6927ec4aa7eab68ea958fb1b9f2950c681f7
mellsvs <meltemsavas192@gmail.com>
2024-04-19 15:30:02 +0300

    employee design

--- src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmsBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
